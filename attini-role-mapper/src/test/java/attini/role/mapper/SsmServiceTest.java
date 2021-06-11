9dacbb58c6be35ba610efdf58a1cf0e6eeab3751
oscarostrand <oscar.ostrand@gmail.com>
2021-06-11 12:04:43 +0200

    refactore some and added notes

--- attini-role-mapper/src/test/java/attini/role/mapper/SsmServiceTest.java
+++ attini-role-mapper/src/test/java/attini/role/mapper/SsmServiceTest.java
@@ -1,30 +1,38 @@
 
 package attini.role.mapper;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static software.amazon.awssdk.regions.Region.EU_NORTH_1;
+import static software.amazon.awssdk.regions.Region.EU_WEST_1;
+import static software.amazon.awssdk.regions.Region.EU_WEST_2;
+import static software.amazon.awssdk.regions.Region.US_EAST_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import javax.inject.Inject;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
-import javax.inject.Inject;
-
-import software.amazon.awssdk.services.ssm.model.*;
+import software.amazon.awssdk.services.ssm.model.GetParametersByPathRequest;
+import software.amazon.awssdk.services.ssm.model.GetParametersByPathResponse;
+import software.amazon.awssdk.services.ssm.model.Parameter;
 import software.amazon.awssdk.services.ssm.paginators.GetParametersByPathIterable;
 
 @ExtendWith(MockitoExtension.class)
 public class SsmServiceTest {
-    
+
     @Inject
     SsmService ssmService;
 
@@ -39,30 +47,32 @@ public class SsmServiceTest {
         ssmService = new SsmService(ssmClient);
     }
 
-    /**
-     *
-     */
     @Test
     public void getAllRegionsTest() {
-        Mockito.when(ssmClient.getParametersByPathPaginator(Mockito.any(GetParametersByPathRequest.class)))
-            .thenReturn(getParametersByPathIterable);
+        when(ssmClient.getParametersByPathPaginator(any(GetParametersByPathRequest.class)))
+                .thenReturn(getParametersByPathIterable);
+
         GetParametersByPathResponse.Builder responseBuilder = GetParametersByPathResponse.builder();
-        //responseBuilder.....
-        //responseBuilder.parameters();
 
-        Parameter.Builder paramBuilder = Parameter.builder();
+        Stream<GetParametersByPathResponse> responseStream = Stream.of(
+                responseBuilder.parameters(toParam("eu-west-1"),
+                                           toParam("eu-north-1")).build(), //då det kan komma olika många regioner i varje paginering är det värt att ha med det i sitt test
+                responseBuilder.parameters(toParam("us-east-1")).build());
 
-        Stream<GetParametersByPathResponse> s = Stream.of(
-                (responseBuilder.parameters(paramBuilder.value("eu-west-1").build()).build()),
-                (responseBuilder.parameters(paramBuilder.value("us-east-1").build()).build()));
-
-        Mockito.when(getParametersByPathIterable.stream()).thenReturn(s);
-
+        when(getParametersByPathIterable.stream()).thenReturn(responseStream);
 
         List<Region> regions = ssmService.getAllRegions();
-        assertNotEquals(new ArrayList<Region>(), regions);
-        assertTrue(regions.contains(Region.EU_WEST_1) && regions.contains(Region.US_EAST_1));
-        assertFalse(regions.contains(Region.EU_WEST_2));
 
+        assertNotEquals(new ArrayList<Region>(), regions); // onödigt, behöver inte gämföra med en tom lista, om man vill kan man kolla att listan inte är tom men bättre att kolla att den har rätt size, se nedan
+        assertEquals(3,regions.size());
+        assertTrue(regions.containsAll(List.of(EU_WEST_1, US_EAST_1, EU_NORTH_1)));
+        assertFalse(regions.contains(EU_WEST_2));
+
+    }
+
+    //Lägg till fler test med felhantering
+
+    private static Parameter toParam(String value) {
+        return Parameter.builder().value(value).build();
     }
 }
