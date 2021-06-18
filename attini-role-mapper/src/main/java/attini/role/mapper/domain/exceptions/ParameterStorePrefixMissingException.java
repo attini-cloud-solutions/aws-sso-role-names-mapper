d090e837ebdef376e46d95e6722fe5f4897b80bf
backstromjoel <joel.backstrom3@gmail.com>
2021-06-18 13:51:49 +0200

    Add getter of environmentVariable for prefix and cleanup code
    Co-authored-by: Anton Eriksson <antoneriksson978@gmail.com>

---
+++ attini-role-mapper/src/main/java/attini/role/mapper/domain/exceptions/ParameterStorePrefixMissingException.java
@@ -0,0 +1,7 @@
+package attini.role.mapper.domain.exceptions;
+
+public class ParameterStorePrefixMissingException extends RuntimeException {
+    public ParameterStorePrefixMissingException(String s) {
+        super(s);
+    }
+}
