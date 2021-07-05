ae5bf79fce1fabc0d3c72cf0d108143f38462a48
oscarostrand <oscar.ostrand@gmail.com>
2021-07-05 11:22:49 +0200

    fixed error when it fails if there are no parameters

--- src/test/java/aws/sso/role/names/mapper/facades/SsmFacadeTest.java
+++ src/test/java/aws/sso/role/names/mapper/facades/SsmFacadeTest.java
@@ -4,9 +4,9 @@ import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static software.amazon.awssdk.regions.Region.EU_NORTH_1;
@@ -31,6 +31,7 @@ import aws.sso.role.names.mapper.domain.ParameterStorePrefix;
 import aws.sso.role.names.mapper.domain.PermissionSetName;
 import aws.sso.role.names.mapper.domain.SsmDeleteParametersRequest;
 import aws.sso.role.names.mapper.domain.SsmPutParameterRequest;
+import aws.sso.role.names.mapper.domain.exceptions.CouldNotGetParametersException;
 import aws.sso.role.names.mapper.factories.SsmClientFactory;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
@@ -84,7 +85,7 @@ public class SsmFacadeTest {
 
         Stream<GetParametersByPathResponse> responseStream = Stream.of(
                 responseBuilder.parameters(toParam("eu-west-1"),
-                        toParam("eu-north-1")).build(),
+                                           toParam("eu-north-1")).build(),
                 responseBuilder.parameters(toParam("us-east-1")).build());
 
         when(getParametersByPathIterableMock.stream()).thenReturn(responseStream);
@@ -97,7 +98,7 @@ public class SsmFacadeTest {
     }
 
     @Test
-    public void getParameters_SsmError_ShouldReturnEmptySet() {
+    public void getParameters_SsmError_ShouldThrowCouldNotGetParametersException() {
         when(environmentVariablesMock.getParameterStorePrefix()).thenReturn(ParameterStorePrefix.create("/test/"));
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
         when(ssmClientMock.getParametersByPathPaginator(any(GetParametersByPathRequest.class)))
@@ -105,47 +106,53 @@ public class SsmFacadeTest {
         when(getParametersByPathIterableMock.stream()).thenThrow(SsmException.builder().build());
 
         // Irrelevant what Region is used.
-        assertTrue(ssmFacade.getParameters(Region.AP_NORTHEAST_3).isEmpty());
+        assertThrows(CouldNotGetParametersException.class,()-> ssmFacade.getParameters(Region.AP_NORTHEAST_3));
     }
 
     @Test
-    public void deleteParameters_SsmError_ShouldReturnFalse() {
+    public void deleteParameters_SsmError_ShouldReturnEmptySet() {
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
         when(ssmClientMock.deleteParameters(any(DeleteParametersRequest.class))).thenThrow(SsmException.class);
 
         // Irrelevant what Region or Set is used.
-        assertFalse(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(Region.EU_WEST_3, emptySet())));
+        assertTrue(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(Region.EU_WEST_3, emptySet()))
+                            .isEmpty());
     }
 
     @Test
-    public void deleteParameters_SsmError_ShouldReturnTrue() {
+    public void deleteParameters_emptyResponse_ShouldReturnEmptySet() {
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
-        when(ssmClientMock.deleteParameters(any(DeleteParametersRequest.class))).thenReturn(DeleteParametersResponse.builder().build());
+        when(ssmClientMock.deleteParameters(any(DeleteParametersRequest.class))).thenReturn(DeleteParametersResponse.builder()
+                                                                                                                    .build());
 
         // Irrelevant what Region or Set is used.
-        assertTrue(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(Region.EU_WEST_3, emptySet())));
+        assertTrue(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(EU_WEST_3, emptySet())).isEmpty());
     }
 
     @Test
-    public void deleteParameters_EmptyRequest_ShouldReturnFalse() {
+    public void deleteParameters_EmptyRequest_ShouldReturnEmptySet() {
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
         when(ssmClientMock.deleteParameters(DeleteParametersRequest.builder().names(emptySet()).build()))
                 .thenThrow(SsmException.class);
-        assertFalse(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(EU_WEST_3, emptySet())));
+        assertTrue(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(EU_WEST_3, emptySet())).isEmpty());
     }
 
     @Test
-    public void deleteParameters_ValidRequest_ShouldReturnTrue() {
+    public void deleteParameters_ValidRequest_ShouldReturnDeletedParameter() {
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
-        List<String> names = List.of("name1", "name2", "name3");
-        lenient().when(ssmClientMock.deleteParameters(DeleteParametersRequest.builder().names(names).build()))
+        Set<String> names = Set.of("name1", "name2", "name3");
+        when(ssmClientMock.deleteParameters(any(DeleteParametersRequest.class)))
                 .thenReturn(DeleteParametersResponse.builder().deletedParameters(names).build());
+
         Set<ParameterName> params = names.stream().map(ParameterName::create).collect(toSet());
-        assertTrue(ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(EU_WEST_3, params)));
+        Set<ParameterName> deletedParameters = ssmFacade.deleteParameters(SsmDeleteParametersRequest.create(EU_WEST_3,
+                                                                                                            params));
+        assertFalse(deletedParameters.isEmpty());
+        assertEquals(params, deletedParameters);
     }
 
     @Test
-    public void putParameters_SsmError_ShouldReturnFalse() {
+    public void putParameter_SsmError_ShouldReturnFalse() {
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
         when(ssmClientMock.putParameter(any(PutParameterRequest.class))).thenThrow(SsmException.class);
 
@@ -154,19 +161,22 @@ public class SsmFacadeTest {
                 Region.EU_NORTH_1,
                 ParameterName.create("/attini/aws-sso-role-names-mapper/AdministratorAccess"),
                 PermissionSetName.create("AWSReservedSSO_DatabaseAdministrator_e90c045f34e6a0ad"),
-                Arn.create("arn:aws:iam::855066048591:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_Billing_c8106817c1780052"))));
+                Arn.create(
+                        "arn:aws:iam::855066048591:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_Billing_c8106817c1780052"))));
     }
 
     @Test
-    public void putParameters_SsmError_ShouldReturnTrue() {
+    public void putParameter_SsmError_ShouldReturnTrue() {
         when(ssmClientFactoryMock.createSsmClient(any(Region.class))).thenReturn(ssmClientMock);
-        when(ssmClientMock.putParameter(any(PutParameterRequest.class))).thenReturn(PutParameterResponse.builder().build());
+        when(ssmClientMock.putParameter(any(PutParameterRequest.class))).thenReturn(PutParameterResponse.builder()
+                                                                                                        .build());
 
         // Irrelevant what arguments are used.
         assertTrue(ssmFacade.putParameter(SsmPutParameterRequest.create(
                 Region.EU_NORTH_1,
                 ParameterName.create("/attini/aws-sso-role-names-mapper/AdministratorAccess"),
                 PermissionSetName.create("AWSReservedSSO_DatabaseAdministrator_e90c045f34e6a0ad"),
-                Arn.create("arn:aws:iam::855066048591:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_Billing_c8106817c1780052"))));
+                Arn.create(
+                        "arn:aws:iam::855066048591:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_Billing_c8106817c1780052"))));
     }
 }
